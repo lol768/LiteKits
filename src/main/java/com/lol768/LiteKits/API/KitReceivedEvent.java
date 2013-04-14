04944eae5b157c8a473e0fab161e815ef2be5c4f
lol768 <support@technostripe.com>
2013-04-14 20:25:26 +0100

    Add events for API

---
+++ src/main/java/com/lol768/LiteKits/API/KitReceivedEvent.java
@@ -0,0 +1,42 @@
+package com.lol768.LiteKits.API;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class KitReceivedEvent extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+    private Boolean cancel = false;
+    private Player p;
+    private String kitName;
+    
+    public KitReceivedEvent(Player p, String kitName) {
+        this.p = p;
+        this.kitName = kitName;
+    }
+    
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+    
+    public void setCancelled(Boolean cancelled) {
+        this.cancel = cancelled;
+    }
+    
+    public boolean isCancelled() {
+        return this.cancel;
+    }
+    
+    public Player getPlayer() {
+        return this.p;
+    }
+    
+    public String getKitName() {
+        return this.kitName;
+    }
+}
